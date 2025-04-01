edf17ee8d2506356e8c83ab252570068fb442fb6
superartificial <clemw@hotmail.com>
2025-04-01 14:37:45 +1300

    First Commit: Setup Patient ms, create Patient Entity

---
+++ patient-service/src/main/java/nz/com/patient_service/PatientServiceApplication.java
@@ -0,0 +1,12 @@
+package nz.com.patient_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientServiceApplication.class, args);
+    }
+}
